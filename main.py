6f4b3598172666bc273da5d45c35a4710df3aa92
My Name <myEmail@example.com>
2022-11-17 18:11:31 +0500

    New print commit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def main():
-    print('My first git program')
-    print('And I change it every day')
+    print('My first git-repo')
 
 
 if __name__ == '__main__':
     main()
-
-#40382f581e45dc4e795bea3dfc98f158c71a820a
